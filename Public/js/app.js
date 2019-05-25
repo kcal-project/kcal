51e4c46b4eae025349d910dfa19452584e447359
bjwine0 <bradsmilek@gmail.com>
2019-05-25 10:07:17 -0700

    working on constructor // changes to file paths and convention

--- Public/js/app.js
+++ Public/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(
-  $('#button2').on('click', getBmr)
+  $('#button1').on('click', getBmr)
 );
 
 function getBmr(){
@@ -8,6 +8,8 @@ function getBmr(){
   let age = parseFloat($('#getAge').val());
   let sex = $('input[name=\'sex\']:checked').val();
   let activity = parseFloat($('#getActivity').val());
+  let goal = parseFloat($('#getGoal').val());
+  let loss = $('input[name=\'loss\']:checked').val();
   console.log(activity);
   let bmrWithoutActivity = 0;
   if (sex === 'male'){
@@ -16,16 +18,25 @@ function getBmr(){
   else{
     bmrWithoutActivity = (10*(weight/2.205) + (6.25*(height*2.54)) - (5*age) - 161);
   }
-  let completeBmr = bmrWithoutActivity * activity;
 
-  console.log(height, weight, age, sex, activity);
+  let completeBmr = Math.floor(bmrWithoutActivity * activity);
+
+  if (loss === 'mild'){
+    // return completeBmr -215;
+    alert(completeBmr -215);
+  }
+  if (loss === 'moderate'){
+    // return completeBmr -500;
+    alert(completeBmr -500);
+
+  }
+  if (loss === 'extreme'){
+    // return completeBmr -1000;
+    alert(completeBmr - 1000);
+
+  }
 
 
-  // evalActivity * ((10 * weight) + (6.25 * height) - (5 * age) - 161);
+  console.log(height, weight, age, sex, activity, loss, goal);
 
-
-  let string = 'BMR: ' + bmrWithoutActivity + ' calories per day';
-  let string2 = 'completeBMR: ' + completeBmr + ' calories per day';
-  alert(string);
-  alert(string2);
 }
